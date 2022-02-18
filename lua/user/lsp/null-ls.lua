268e45c14ead928e913a8ee5de065d8e189ee41a
kahgeh <kahgeh@hotmail.com>
2022-02-18 23:39:44 +1100

    remove no-semi formatting and add eslint

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,9 +11,10 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    formatting.stylelint,
+		formatting.stylelint,
+		diagnostics.eslint,
 	},
 })
