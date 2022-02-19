0561460a4a02279dd3843a959d4ee8c156520a4d
kahgeh <kahgeh@hotmail.com>
2022-02-19 15:03:15 +1100

    fix denols setup

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -11,33 +11,32 @@ lsp_installer.on_server_ready(function(server)
 		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
+	if server.name == "jsonls" then
+		local jsonls_opts = require("user.lsp.settings.jsonls")
+		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	end
 
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
+	if server.name == "sumneko_lua" then
+		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	end
 
-  if server.name == "tsserver" then 
-    local tsserver_opts = require("user.lsp.settings.tsserver")
-    opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
-  end
-  
-  if server.name == "deno_ls" then 
-    local denols_opts = require("user.lsp.settings.denols")
-    opts = vim.tbl_deep_extend("force", denols_opts, opts)
-  end
+	if server.name == "tsserver" then
+		local tsserver_opts = require("user.lsp.settings.tsserver")
+		opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
+	end
 
-  if server.name == "powershell_es" then
-    local powershelles_opts = require("user.lsp.settings.powershell_es")
-    opts = vim.tbl_deep_extend("force", powershelles_opts, opts)
-  end
+	if server.name == "denols" then
+		local denols_opts = require("user.lsp.settings.denols")
+		opts = vim.tbl_deep_extend("force", denols_opts, opts)
+	end
+
+	if server.name == "powershell_es" then
+		local powershelles_opts = require("user.lsp.settings.powershell_es")
+		opts = vim.tbl_deep_extend("force", powershelles_opts, opts)
+	end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
 end)
-
