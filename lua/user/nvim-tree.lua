913c0b70fe6ef7b85de9c889b66a67a215516f30
kahgeh <kahgeh@hotmail.com>
2022-08-27 17:54:16 +1000

    align nvim tree settings update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -29,10 +29,6 @@ nvim_tree.setup({
 	open_on_tab = false,
 	hijack_cursor = false,
 	update_cwd = true,
-	update_to_buf_dir = {
-		enable = true,
-		auto_open = true,
-	},
 	diagnostics = {
 		enable = true,
 		icons = {
@@ -65,7 +61,6 @@ nvim_tree.setup({
 		height = 30,
 		hide_root_folder = false,
 		side = "left",
-		auto_resize = true,
 		mappings = {
 			custom_only = false,
 			list = {
