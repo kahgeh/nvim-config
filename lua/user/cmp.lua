f0ee73c130563c6b84085dd704773ead37c46d61
kahgeh <kahgeh@hotmail.com>
2022-12-27 22:28:11 +1100

    use arrow keys for completion

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -52,8 +52,8 @@ cmp.setup {
     end,
   },
   mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<up>"] = cmp.mapping.select_prev_item(),
+		["<down>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
