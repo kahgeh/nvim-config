e27ccb429f04fc750a36cedcb0a7afb2f20e9069
kahgeh <kahgeh@hotmail.com>
2022-12-27 22:29:41 +1100

    use F for pattern hop

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -62,8 +62,10 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Buffer navigation --
 keymap("n", "f", ":HopWord<cr>", opts)
+keymap("n", "F", ":HopPattern<cr>", opts)
 keymap("n", "<A-BS>", "bdw", opts)
 keymap("n", "d", '"dd', opts) -- delete shouldn't move update the default register
+keymap("n", "c", '"dc', opts) -- delete shouldn't move update the default register
 keymap("i", "<A-BS>", "<ESC>hdaw", opts)
 keymap("i", "<S-BS>", "<ESC>hdaw", opts)
 
