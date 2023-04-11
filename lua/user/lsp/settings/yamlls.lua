d49fa70541afcc6d80061ab78ad5a9d88cf535b6
kahgeh <kahgeh@hotmail.com>
2023-04-11 19:12:55 +1000

    use schemastore's schemas

---
+++ lua/user/lsp/settings/yamlls.lua
@@ -0,0 +1,12 @@
+local status_ok, schemastore = pcall(require, "schemastore")
+if not status_ok then
+	return
+end
+
+return {
+	settings = {
+		yaml = {
+			schemas = schemastore.yaml.schemas(),
+		},
+	},
+}
