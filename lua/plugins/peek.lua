d4cdb1cf558ee0c1a1b65bd0a11cf6b7a435f336
kahgeh <kahgeh@hotmail.com>
2023-09-17 22:22:20 +1000

    md peek, open normal browser so that it's possible to zoom

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -7,12 +7,13 @@ local M = {
 function M.config()
   require("peek").setup({
     filetype = { "markdown" },
+    app = "browser"
   })
   vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
   vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 end
 
-  function M.build()
+function M.build()
 
 end
 
